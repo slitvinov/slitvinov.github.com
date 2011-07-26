81abb48db29cd19f0e1e3956ea781c89f933d088
Litvinov Sergey <slitvinov@gmail.com>
2011-07-26 11:08:06 +0200

    Add a warning message

--- lammps/pair_dpd/update.sh
+++ lammps/pair_dpd/update.sh
@@ -4,5 +4,6 @@ set -e
 set -u
 
 lmp_srcroot=/scratch/work/lammps-ro
-cp ${lmp_srcroot}/doc/pair_dpd.html index.html
+msg='<H2><font color="red">proposed patch (not implemented in mainstream version)</font> </H2>'
+gawk --lint=fatal -v fl=0 -v msg="${msg}" '{print} (!fl)&&$0~/<HR>/{print $0; print msg; fl=1}' ${lmp_srcroot}/doc/pair_dpd.html > index.html
 cp ${lmp_srcroot}/doc/Eqs/pair_dpd*.jpg Eqs/
